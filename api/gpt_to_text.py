a35d81ef42e2166ff91eb152a74e04ce3d7fe4d0
OlegGlo <olegglotov99@gmail.com>
2024-06-23 10:34:23 -0400

    initial stuff - needs cleaning

---
+++ api/gpt_to_text.py
@@ -0,0 +1,36 @@
+import os
+
+from groq import Groq
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv('./secrets/secrets.env')
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', None)
+
+def get_lyrics_from_gpt(song_name: str, artist_name: str):
+    client = Groq(
+        api_key=os.environ.get("GROQ_API_KEY"),
+    )
+
+    prompt = f"Give me the full complete whole lyrics of the song {song_name} \
+    By {artist_name} \
+    Please only return the lyrics and nothing else. \
+    Do not include the song title or any other text. \
+    Do not even say 'Sure here are the lyrics...' \
+    Do not include any other text in the response such as (chorus, verse, etc.)."
+    
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3-70b-8192",
+        # stream=True
+    )
+
+    print(chat_completion.choices[0].message.content)
+    return(chat_completion.choices[0].message.content)
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+
+from api.gpt_to_text import get_lyrics_from_gpt  # Import your function here
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS
+
+@app.route('/get-lyrics', methods=['POST'])
+def get_lyrics():
+    
+    data = request.json
+    song_name = data['song_name']
+    artist_name = data['artist']
+    lyrics = get_lyrics_from_gpt(song_name, artist_name)
+    return jsonify({'lyrics': lyrics})
+
+if __name__ == '__main__':
+    app.run(debug=True)
